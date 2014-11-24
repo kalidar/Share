6df18de36e1b4d3df7a6bcef0208344c4bb46fab
unknown <k@laptop.com>
2014-11-24 12:54:45 +0100

    Added Stonevault Hunter pets

---
+++ Stonevault Hunter Pet.sql
@@ -0,0 +1,5 @@
+-- Creature id: 45035
+DELETE FROM creature_ai_scripts WHERE creature_id=45035;
+INSERT INTO creature_ai_scripts VALUES 
+('4503501','45035','0','0','100','1','9000','13000','28000','33000','11','3635','1','0','0','0','0','0','0','0','0','0','[PH] Hunter Pet - Cast Crystal Gaze'),
+('4503502','45035','2','0','100','0','40','0','0','0','11','9906','0','0','0','0','0','0','0','0','0','0','[PH] Hunter Pet - Cast Reflection at 40% HP');
