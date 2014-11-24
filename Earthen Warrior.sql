89b20bb13e78b0aee1940081b3dd2b8c8cc55172
unknown <k@laptop.com>
2014-11-24 15:41:23 +0100

    Added Earthen Warrior

---
+++ Earthen Warrior.sql
@@ -0,0 +1,5 @@
+-- Creature id: 45036
+DELETE FROM creature_ai_scripts WHERE creature_id=45036;
+INSERT INTO creature_ai_scripts VALUES 
+('4503601','45036','13','0','100','0','22000','28000','0','0','11','15615','1','0','0','0','0','0','0','0','0','0','[PH] Earthen Warrior - Pummel on Target Spell Cast'),
+('4503602','45036','0','0','100','0','9000','19000','30000','35000','11','15588','0','0','0','0','0','0','0','0','0','0','[PH] Earthen Warrior - Cast Thunderclap');
